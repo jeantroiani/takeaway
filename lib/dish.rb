0c59576786c2d27d69ad32e5a796102bcf79a24f
jeantroiani <digitalguest@gmail.com>
2014-07-20 17:24:57 +0100

    Added a Readme file, and price can be shown and updated for Dish classes.

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,2 +1,15 @@
 class Dish
 
+	def initialize(price=nil)
+		@price = price
+	end
+
+	def price
+		@price
+	end
+
+	def update_price(new_price)
+		@price=new_price
+	end
+
+end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,8 +1,21 @@
 require "./lib/dish"
 
 describe Dish do
+	let (:arepa) {Dish.new}
+		
 
-	it " 
+		it " you can add a price when initialized it" do
+			arepa=Dish.new(5)
+			expect(arepa.price).to eq(5)
+
+		end
+
+		it" can change its price" do
+			arepa=Dish.new(5)
+			arepa.update_price(6)
+			expect(arepa.price).to eq 6
+		end
+
 
 
 
